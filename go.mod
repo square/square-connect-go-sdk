3cdbd5d920b00696523ce8cbaa7edb4feadb8dcc
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2021-03-25 17:20:03 -0700

    Add some tests

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/antihax/optional v1.0.0
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 )
---
+++ patches_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/square/square-connect-go-sdk/swagger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEcomPatch(t *testing.T) {
+	s := swagger.CatalogItem{}
+	// Ensure that Ecom fields exist. As long as the test compiles we are good and don't need any assertions.
+	s.EcomUri = "xyz"
+	s.EcomImageUris = []string{"xyz"}
+	s.EcomAvailable = true
+	s.EcomVisibility = "xyz"
+}
+
+func TestModelCatalogCustomAttributeValuePatch(t *testing.T) {
+	s := swagger.CatalogCustomAttributeValue{
+		StringValue:  "",
+		BooleanValue: false,
+	}
+	// Ensure that omitempty has been removed
+	json, err := json.Marshal(s)
+	require.NoError(t, err)
+	require.Equal(t, "{\"string_value\":\"\",\"boolean_value\":false}", string(json))
+}
