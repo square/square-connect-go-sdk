fed62e32c1ed7e116aa8b3a8cf62689bafd43077
Sides <sides@getansa.com>
2022-11-15 20:28:42 -0800

    Do not omitempty on a valid zero value for `Amount`

--- swagger/model_money.go
+++ swagger/model_money.go
@@ -12,6 +12,6 @@ package swagger
 // Represents an amount of money. `Money` fields can be signed or unsigned. Fields that do not explicitly define whether they are signed or unsigned are considered unsigned and can only hold positive amounts. For signed fields, the sign of the value indicates the purpose of the money transfer. See [Working with Monetary Amounts](https://developer.squareup.com/docs/build-basics/working-with-monetary-amounts) for more information.
 type Money struct {
 	// The amount of money, in the smallest denomination of the currency indicated by `currency`. For example, when `currency` is `USD`, `amount` is in cents. Monetary amounts can be positive or negative. See the specific field description to determine the meaning of the sign in a particular case.
-	Amount   int64     `json:"amount,omitempty"`
+	Amount   int64     `json:"amount"`
 	Currency *Currency `json:"currency,omitempty"`
 }
